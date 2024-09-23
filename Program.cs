a49667e25f899f4c2080ad002b8861be7db9fc35
Selvamaz <selvarajumup@gmail.com>
2024-09-23 08:39:43 +0530

    V 0.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
